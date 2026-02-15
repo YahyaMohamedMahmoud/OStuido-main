cc7a24c7c21821ed27e1484b7350efe695e97253
YahyaMohamedMahmoud <151754506+YahyaMohamedMahmoud@users.noreply.github.com>
2026-02-15 14:12:27 +0200

    edit-js-dots-in mobile

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ function resize() {
     centerY = height / 2;
 
     if (width <= 600) {
-      radius = Math.min(width, height) * 0.60;
+      radius = Math.min(width, height) * 0.65;
     }else if (width <= 767) {
   
         radius = Math.min(width, height) * 0.40;
